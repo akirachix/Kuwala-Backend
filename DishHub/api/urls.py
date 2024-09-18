bfbbc4692076f4952f248e3b5a021e20edd1e0be
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-09-18 16:54:50 +0300

    made changes

--- DishHub/api/urls.py
+++ DishHub/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RecipeListView,RecipeDetailView
-
+from .views import CreateRecipeView, RecipeListView, FetchRecipeView, SearchRecipeView
 
 urlpatterns = [
-    path("recipe/", RecipeListView.as_view(), name="recipe_list_view"),
-    path("recipe/<int:id>/",RecipeDetailView.as_view(),name="recipe_detail_view")
+    path('recipes/', RecipeListView.as_view(), name='recipe_list'),  
+    path('recipes/create/', CreateRecipeView.as_view(), name='create_recipe'),
+    path('recipes/<int:recipe_id>/', FetchRecipeView.as_view(), name='fetch_recipe'),  
+    path('recipes/search/', SearchRecipeView.as_view(), name='search_recipes'),  
 ]
