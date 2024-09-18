abdf69692eb67fe63d9ef8ddb92518d1cae23303
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-09-18 15:48:41 +0300

    made changes

--- recipes/models.py
+++ recipes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Recipe(models.Model):
     recipe_id = models.CharField(max_length=255, primary_key=True)
     title = models.CharField(max_length=255)
-    ingredients = models.JSONField(default=list)  
+    ingredient = models.JSONField(default=list)  
     instructions = models.TextField()
     image_url = models.URLField(max_length=1000, blank=True, null=True)
 
